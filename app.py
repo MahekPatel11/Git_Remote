36c2887f8c8ff48fd31c5f1d0ae515d5aaeb4ef0
MahekPatel11 <mahekpatel2005@gmail.com>
2026-01-22 22:36:59 +0530

    changes in development branch

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ st.title("Streamlit Capstone Project")
 name = st.text_input("Enter your name:")
 age = st.text_input("Enter your age:")
 
+st.write("This is the extra line.")
+
 if st.button("Submit"):
     st.success(f"Hello {name} of age {age}, welcome to the Streamlit app!")
