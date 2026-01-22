eb9e3947726963af4bbc595064750e8400caa373
MahekPatel11 <mahekpatel2005@gmail.com>
2026-01-22 22:02:29 +0530

    Initial streamlit app done

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Streamlit Capstone Project")
+
+name = st.text_input("Enter your name:")
+age = st.text_input("Enter your age:")
+
+if st.button("Submit"):
+    st.success(f"Hello {name} of age {age}, welcome to the Streamlit app!")
